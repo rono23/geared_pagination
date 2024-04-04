bd0bd07a493eebf3604b8024c3ef20d5312a6008
rono23 <rono23@gmail.com>
2024-04-04 20:01:35 +0900

    Add tests

--- test/page_test.rb
+++ test/page_test.rb
@@ -26,7 +26,62 @@ class GearedPagination::PageTest < ActiveSupport::TestCase
   test "before_last" do
     assert     GearedPagination::Recordset.new(Recording.all, per_page:    1).page(1).before_last?
     assert_not GearedPagination::Recordset.new(Recording.all, per_page: 1000).page(1).before_last?
+    assert_not GearedPagination::Recordset.new(Recording.all, per_page: Recording.all.count).page(1).before_last?
     assert_not GearedPagination::Recordset.new(Recording.none, per_page: 1000).page(2).before_last?
+  end
+
+  test "before_last with `per_page: 2`" do
+    # Easily verify that each page is correct by removing unnecessary records.
+    Recording.where("id > 7").destroy_all
+    options = { per_page: 2, ordered_by: { id: :desc } }
+    first_page = GearedPagination::Recordset.new(Recording.all, **options)
+      .page(GearedPagination::Cursor.encode(page_number: 1))
+    second_page = GearedPagination::Recordset.new(Recording.all, **options).page(first_page.next_param)
+    third_page = GearedPagination::Recordset.new(Recording.all, **options).page(second_page.next_param)
+    fourth_page = GearedPagination::Recordset.new(Recording.all, **options).page(third_page.next_param)
+    assert_equal [7, 6], first_page.records.ids
+    assert_equal [5, 4], second_page.records.ids
+    assert_equal [3, 2], third_page.records.ids
+    assert_equal [1], fourth_page.records.ids
+    assert       first_page.before_last?
+    assert       second_page.before_last?
+    assert       third_page.before_last?
+    assert_not   fourth_page.before_last?
+
+    Recording.first.destroy!
+    second_page = GearedPagination::Recordset.new(Recording.all, **options).page(first_page.next_param)
+    third_page = GearedPagination::Recordset.new(Recording.all, **options).page(second_page.next_param)
+    fourth_page = GearedPagination::Recordset.new(Recording.all, **options).page(third_page.next_param)
+    assert_equal [5, 4], second_page.records.ids
+    assert_equal [3, 2], third_page.records.ids
+    assert_equal [], fourth_page.records.ids
+    assert       second_page.before_last?
+    assert_not   third_page.before_last?
+    assert_not   fourth_page.before_last?
+  end
+
+  test "before_last with `per_page: 3`" do
+    # Easily verify that each page is correct by removing unnecessary records.
+    Recording.where("id > 7").destroy_all
+    options = { per_page: 3, ordered_by: { id: :desc } }
+    first_page = GearedPagination::Recordset.new(Recording.all, **options)
+      .page(GearedPagination::Cursor.encode(page_number: 1))
+    second_page = GearedPagination::Recordset.new(Recording.all, **options).page(first_page.next_param)
+    third_page = GearedPagination::Recordset.new(Recording.all, **options).page(second_page.next_param)
+    assert_equal [7, 6, 5], first_page.records.ids
+    assert_equal [4, 3, 2], second_page.records.ids
+    assert_equal [1], third_page.records.ids
+    assert       first_page.before_last?
+    assert       second_page.before_last?
+    assert_not   third_page.before_last?
+
+    Recording.last.destroy!
+    second_page = GearedPagination::Recordset.new(Recording.all, **options).page(first_page.next_param)
+    third_page = GearedPagination::Recordset.new(Recording.all, **options).page(second_page.next_param)
+    assert_equal [4, 3, 2], second_page.records.ids
+    assert_equal [1], third_page.records.ids
+    assert_not   second_page.before_last?
+    assert_not   third_page.before_last?
   end
 
   test "next offset param" do
